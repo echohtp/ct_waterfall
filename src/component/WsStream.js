f65ffe19427947a0a3b4fa539666ba9a1ba7a130
Jason Schorr <jason@spyglassltd.com>
2020-05-03 15:05:56 -0400

    added domain name resolution enrichinment (client side)

--- src/component/WsStream.js
+++ src/component/WsStream.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import Websocket from 'react-websocket';
 import { MDBDataTable } from 'mdbreact';
-
 import { getPublicSuffix } from 'tldjs';
+import Axios from 'axios';
 
 class WsStream extends React.Component {
 
@@ -63,13 +63,30 @@ class WsStream extends React.Component {
         message: message,
         domain: message["data"]["leaf_cert"]["all_domains"][i],
         issuer: message['data']['chain'][0]['subject']['CN'],
-        entropy: this.entropy(message["data"]["leaf_cert"]["all_domains"][i])
+        entropy: this.entropy(message["data"]["leaf_cert"]["all_domains"][i]),
+        ip: []
 
       }
       //getPublicSuffix
-      if (t_dmn["domain"].charAt(0) !== "*"){
-        tldData[getPublicSuffix(message["data"]["leaf_cert"]["all_domains"][i])] = (typeof tldData[getPublicSuffix(message["data"]["leaf_cert"]["all_domains"][i])] != 'undefined' && tldData[getPublicSuffix(message["data"]["leaf_cert"]["all_domains"][i])] instanceof Array) ? tldData[getPublicSuffix(message["data"]["leaf_cert"]["all_domains"][i])] : []
-        tldData[getPublicSuffix(message["data"]["leaf_cert"]["all_domains"][i])].push(t_dmn)
+      if (t_dmn["domain"].charAt(0) !== "*") {
+
+        Axios.get('https://dns.google.com/resolve?name=' + t_dmn["domain"] + '&type=A').then(function (response) {
+          // handle success
+          response.data.Answer.map((ip)=>{
+            t_dmn["ip"].push(ip["data"])
+            return 1
+          })
+        })
+          .catch(function (error) {
+            // handle error
+            console.log(error);
+          })
+          .then(function () {
+            // always executed
+            tldData[getPublicSuffix(message["data"]["leaf_cert"]["all_domains"][i])] = (typeof tldData[getPublicSuffix(message["data"]["leaf_cert"]["all_domains"][i])] != 'undefined' && tldData[getPublicSuffix(message["data"]["leaf_cert"]["all_domains"][i])] instanceof Array) ? tldData[getPublicSuffix(message["data"]["leaf_cert"]["all_domains"][i])] : []
+          tldData[getPublicSuffix(message["data"]["leaf_cert"]["all_domains"][i])].push(t_dmn)
+          });
+
       }
       iData[message['data']['chain'][0]['subject']['CN']] = (typeof iData[message['data']['chain'][0]['subject']['CN']] != 'undefined' && iData[message['data']['chain'][0]['subject']['CN']] instanceof Array) ? iData[message['data']['chain'][0]['subject']['CN']] : []
       iData[message['data']['chain'][0]['subject']['CN']].push(t_dmn)
@@ -85,85 +102,91 @@ class WsStream extends React.Component {
     this.setState({ count: nc, done: done });
   }
 
-    handleInputChange(e){
-      if (e.target.name === "tlds") {
-        let filter_arr = this.state.filter_tlds
-        if (filter_arr.includes(e.target.id) === true){
-          let idx = filter_arr.indexOf(e.target.id)
-          filter_arr.splice(idx, 1)
-        }else{
-          filter_arr.push(e.target.id)
-        }
-        this.setState({filter_tlds: filter_arr})
+  handleInputChange(e) {
+    if (e.target.name === "tlds") {
+      let filter_arr = this.state.filter_tlds
+      if (filter_arr.includes(e.target.id) === true) {
+        let idx = filter_arr.indexOf(e.target.id)
+        filter_arr.splice(idx, 1)
+      } else {
+        filter_arr.push(e.target.id)
       }
-      if (e.target.name === "issuers") {
-        let filter_arr = this.state.filter_issuers
-        if (filter_arr.includes(e.target.id) === true){
-          let idx = filter_arr.indexOf(e.target.id)
-          filter_arr.splice(idx, 1)
-        }else{
-          filter_arr.push(e.target.id)
-        }
-        this.setState({filter_issuers: filter_arr})
-      }
-      
-      
-      console.log(e.target.id)
+      this.setState({ filter_tlds: filter_arr })
     }
+    if (e.target.name === "issuers") {
+      let filter_arr = this.state.filter_issuers
+      if (filter_arr.includes(e.target.id) === true) {
+        let idx = filter_arr.indexOf(e.target.id)
+        filter_arr.splice(idx, 1)
+      } else {
+        filter_arr.push(e.target.id)
+      }
+      this.setState({ filter_issuers: filter_arr })
+    }
+
+
+    console.log(e.target.id)
+  }
 
   render() {
 
 
     let rows = []
-    if ( this.state.filter_issuers.length > 0 || this.state.filter_tlds.length > 0 ){
+    if (this.state.filter_issuers.length > 0 || this.state.filter_tlds.length > 0) {
       let tArr = []
-      this.state.filter_issuers.map((i)=>{
+      this.state.filter_issuers.map((i) => {
         tArr.push(...this.state.issuers[i])
         return 1
       })
 
-      this.state.filter_tlds.map((i)=>{
+      this.state.filter_tlds.map((i) => {
         tArr.push(...this.state.tlds[i])
         return 1
       })
-      tArr.map((i) => { rows.push({ "domain": [i['domain'], <a href={"https://" + i['domain']} rel="noopener noreferrer" target="_blank"> <span role="img" aria-label="visit this site">⤴️</span></a>], issuer: i["issuer"], entropy: i['entropy'], not_before: i["not_before"], not_after: i["not_after"], "vt": <a href={"https://www.virustotal.com/gui/search/" + i['domain']} rel="noopener noreferrer" target="_blank"> <span role="img" aria-label="to vt">🦠❓</span></a> }); return 1; })
-    }else{
-      this.state.tData.map((i) => { rows.push({ "domain": [i['domain'], <a href={"https://" + i['domain']} rel="noopener noreferrer" target="_blank"> <span role="img" aria-label="visit this site">⤴️</span></a>], issuer: i["issuer"], entropy: i['entropy'], not_before: i["not_before"], not_after: i["not_after"], "vt": <a href={"https://www.virustotal.com/gui/search/" + i['domain']} rel="noopener noreferrer" target="_blank"> <span role="img" aria-label="to vt">🦠❓</span></a> }); return 1; })
+      tArr.map((i) => { rows.push({ "domain": [i['domain'], <a href={"https://" + i['domain']} rel="noopener noreferrer" target="_blank"> <span role="img" aria-label="visit this site">⤴️</span></a>], ip: i["ip"].join(), issuer: i["issuer"], entropy: i['entropy'], not_before: i["not_before"], not_after: i["not_after"], "vt": <a href={"https://www.virustotal.com/gui/search/" + i['domain']} rel="noopener noreferrer" target="_blank"> <span role="img" aria-label="to vt">🦠❓</span></a> }); return 1; })
+    } else {
+      this.state.tData.map((i) => { rows.push({ "domain": [i['domain'], <a href={"https://" + i['domain']} rel="noopener noreferrer" target="_blank"> <span role="img" aria-label="visit this site">⤴️</span></a>], ip: i["ip"].join(), issuer: i["issuer"], entropy: i['entropy'], not_before: i["not_before"], not_after: i["not_after"], "vt": <a href={"https://www.virustotal.com/gui/search/" + i['domain']} rel="noopener noreferrer" target="_blank"> <span role="img" aria-label="to vt">🦠❓</span></a> }); return 1; })
     }
     const data = {
       columns: [
         {
-          label: ['domain',<br/>, <small>filter on TLDs using the widget</small>],
+          label: ['domain', <br />, <small>filter on TLDs using the widget</small>],
           field: 'domain',
           sort: 'asc',
           width: 100
         },
         {
-          label: ['issuer',<br/>, <small>certificate issuer</small>],
+          label: ['ip', <br />, <small>ip addresses associated with domain</small>],
+          field: 'ip',
+          sort: 'asc',
+          width: 100
+        },
+        {
+          label: ['issuer', <br />, <small>certificate issuer</small>],
           field: 'issuer',
           sort: 'asc',
           width: 100
         },
         {
-          label: ['not_before',<br/>, <small>not valid before date</small>],
+          label: ['not_before', <br />, <small>not valid before date</small>],
           field: 'not_before',
           sort: 'asc',
           width: 100
         },
         {
-          label: ['not_after',<br/>, <small>not valid after date</small>],
+          label: ['not_after', <br />, <small>not valid after date</small>],
           field: 'not_after',
           sort: 'asc',
           width: 100
         },
         {
-          label: ["entropy",<br/>, <small>higher numbers = greater entropy</small>],
+          label: ["entropy", <br />, <small>higher numbers = greater entropy</small>],
           field: "entropy",
           sort: "asc",
           width: 100
         },
         {
-          label: ["virustotal",<br/>, <small>check the domain</small>],
+          label: ["virustotal", <br />, <small>check the domain</small>],
           field: "vt",
           sort: "asc",
           width: 100
@@ -176,14 +199,14 @@ class WsStream extends React.Component {
     return (
 
       <div style={{ width: "98%", paddingLeft: "1%" }}>
-        
+
         {
           !this.state.done && <Websocket url='wss://certstream.calidog.io' onMessage={this.handleData.bind(this)} />
         }
 
         {!this.state.done && <div><div className="loadingio-spinner-cube-fpz0k51mnpw"><div className="ldio-x6fsq21hkv">
-<div></div><div></div><div></div><div></div>
-      </div></div> <p>Processing the first <strong>{this.state.count}/{this.state.limit}</strong> certificates</p></div> } 
+          <div></div><div></div><div></div><div></div>
+        </div></div> <p>Processing the first <strong>{this.state.count}/{this.state.limit}</strong> certificates</p></div>}
 
 
         {this.state.done &&
@@ -191,25 +214,25 @@ class WsStream extends React.Component {
 
           <div className="row">
             <div className="col-1">
-              <br/><br/><br/>
-                <small>filter issuers</small>
-              
+              <br /><br /><br />
+              <small>filter issuers</small>
+
               <div className="filter-box">
                 <ul style={{ textDecoration: "none", textJustify: "left" }}>
-                  {Object.keys(this.state.issuers).map((i) => (<div className="form-check" key={Math.random()}><input className="form-check-input" name="issuers" type="checkbox" checked={this.state.filter_issuers.includes(i) ? true : false } id={i} onChange={this.handleInputChange} /><label className="form-check-label" >{i}</label></div>))}
+                  {Object.keys(this.state.issuers).map((i) => (<div className="form-check" key={Math.random()}><input className="form-check-input" name="issuers" type="checkbox" checked={this.state.filter_issuers.includes(i) ? true : false} id={i} onChange={this.handleInputChange} /><label className="form-check-label" >{i}</label></div>))}
                 </ul>
               </div>
               <hr className="filter-hr" />
-                <small>filter TLDs</small>
-              
+              <small>filter TLDs</small>
+
               <div className="filter-box">
                 <ul style={{ textDecoration: "none", textJustify: "left" }}>
-                  {Object.keys(this.state.tlds).map((i) => (<div className="form-check" key={Math.random()}><input className="form-check-input"  name="tlds" type="checkbox" checked={this.state.filter_tlds.includes(i) ? true : false } id={i} onChange={this.handleInputChange} /><label className="form-check-label" >.{i}</label></div>))}
+                  {Object.keys(this.state.tlds).map((i) => (<div className="form-check" key={Math.random()}><input className="form-check-input" name="tlds" type="checkbox" checked={this.state.filter_tlds.includes(i) ? true : false} id={i} onChange={this.handleInputChange} /><label className="form-check-label" >.{i}</label></div>))}
                 </ul>
               </div>
             </div>
             <div className="col-11">
-              <MDBDataTable  small striped bordered data={data} />
+              <MDBDataTable small striped bordered data={data} />
             </div>
           </div>
         }
